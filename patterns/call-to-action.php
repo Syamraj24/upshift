81f8b5856455a49e25c5dd1048b5c00949729d13
Aparna Adhun <aparnajl437@gmail.com>
2024-04-02 11:27:14 +0530

    Fix : File name changed and added new pattern content

---
+++ patterns/call-to-action.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Title: Call to Action
+ * Slug: upshift/call-to-action
+ * Categories: featured
+ * Keywords: call to action
+ */
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"bottom":"var:preset|spacing|70"}},"border":{"radius":"20px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-primary-background-color has-background" style="border-radius:20px;margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+    <!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-color"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}},"textColor":"text-color","fontSize":"xxxlarge"} -->
+    <h2 class="wp-block-heading has-text-align-center has-text-color-color has-text-color has-link-color has-xxxlarge-font-size" style="padding-bottom:var(--wp--preset--spacing--30)">The latest insights and stories from us</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-color"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"textColor":"text-color","fontSize":"xx-small"} -->
+    <p class="has-text-align-center has-text-color-color has-text-color has-link-color has-xx-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)">Etiam id quam scelerisque, vestibulum dui a, facilisis eros. Nulla mattis varius mauris, in molestie tellus varius et. Sed egestas rutrum ligula,</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons">
+        <!-- wp:button -->
+        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Start a Demo</a></div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+</div>
+<!-- /wp:group -->
