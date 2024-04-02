503104c2195712c1bf83925cf9e12562262fe55d
Aparna Adhun <aparnajl437@gmail.com>
2024-04-02 12:36:50 +0530

    Fix : Correction

--- patterns/call-to-action.php
+++ patterns/call-to-action.php
@@ -10,17 +10,17 @@
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"bottom":"var:preset|spacing|70"}},"border":{"radius":"20px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide has-primary-background-color has-background" style="border-radius:20px;margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
     <!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-color"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}},"textColor":"text-color","fontSize":"xxxlarge"} -->
-    <h2 class="wp-block-heading has-text-align-center has-text-color-color has-text-color has-link-color has-xxxlarge-font-size" style="padding-bottom:var(--wp--preset--spacing--30)">The latest insights and stories from us</h2>
+    <h2 class="wp-block-heading has-text-align-center has-text-color-color has-text-color has-link-color has-xxxlarge-font-size" style="padding-bottom:var(--wp--preset--spacing--30)"><?php esc_html_e('Focus on your business growth with Round', 'upshift'); ?></h2>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-color"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"textColor":"text-color","fontSize":"xx-small"} -->
-    <p class="has-text-align-center has-text-color-color has-text-color has-link-color has-xx-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)">Etiam id quam scelerisque, vestibulum dui a, facilisis eros. Nulla mattis varius mauris, in molestie tellus varius et. Sed egestas rutrum ligula,</p>
+    <p class="has-text-align-center has-text-color-color has-text-color has-link-color has-xx-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)"><?php esc_html_e('Create, build and ship your products real faster. With your own customized portal. showcase your Business like never before', 'upshift'); ?></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
     <div class="wp-block-buttons">
         <!-- wp:button -->
-        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Start a Demo</a></div>
+        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Start a Demo', 'upshift'); ?></a></div>
         <!-- /wp:button -->
     </div>
     <!-- /wp:buttons -->
