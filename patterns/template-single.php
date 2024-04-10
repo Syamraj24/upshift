6fdfed61b4dad263b8779d19ba5a260ce20e92c6
Aparna Adhun <aparnajl437@gmail.com>
2024-04-10 18:44:22 +0530

    Fix : Fixed style issue in single page Add : Added read time short code in single page

--- patterns/template-single.php
+++ patterns/template-single.php
@@ -15,18 +15,18 @@
         <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
             <!-- wp:post-title {"textAlign":"center","level":1,"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40"}},"elements":{"link":{"color":{"text":"var:preset|color|bg-color"}}}},"textColor":"bg-color"} /-->
 
-            <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-            <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+            <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+            <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
                 <!-- wp:post-terms {"term":"category","style":{"spacing":{"padding":{"bottom":"4px","top":"4px","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"backgroundColor":"primary","textColor":"secondary","className":"upshift-post-category","fontSize":"xxx-small"} /-->
 
                 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|bg-color"}}}},"textColor":"bg-color"} -->
                 <p class="has-bg-color-color has-text-color has-link-color">|</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:group {"metadata":{"name":""},"className":"upshift-single-post-author","layout":{"type":"constrained"}} -->
-                <div class="wp-block-group upshift-single-post-author">
+                <!-- wp:group {"metadata":{"name":""},"className":"upshift-single-post-header","layout":{"type":"constrained"}} -->
+                <div class="wp-block-group upshift-single-post-header">
                     <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|bg-color"}}}},"textColor":"bg-color"} -->
-                    <p class="has-bg-color-color has-text-color has-link-color">Written by : </p>
+                    <p class="has-bg-color-color has-text-color has-link-color"><?php esc_html_e('Written by : ', 'upshift'); ?> </p>
                     <!-- /wp:paragraph -->
 
                     <!-- wp:post-author-name /-->
@@ -37,7 +37,17 @@
                 <p class="has-bg-color-color has-text-color has-link-color">|</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:post-date {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":1.8}},"fontSize":"small"} /-->
+
+                <!-- wp:group {"metadata":{"name":""},"className":"upshift-single-post-header","layout":{"type":"constrained"}} -->
+                <div class="wp-block-group upshift-single-post-header">
+                    <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|bg-color"}}}},"textColor":"bg-color"} -->
+                    <p class="has-bg-color-color has-text-color has-link-color"><?php echo do_shortcode('[custom_metabox_shortcode]');
+                                                                                esc_html_e('.read - ', 'upshift'); ?> </p>
+                    <!-- /wp:paragraph -->
+                    <!-- wp:post-date /-->
+
+                </div>
+                <!-- /wp:group -->
 
             </div>
             <!-- /wp:group -->
