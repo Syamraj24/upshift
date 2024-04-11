7d699b800934c4bc12d381fac30b4b2bcc23061c
Aparna Adhun <aparnajl437@gmail.com>
2024-04-11 18:58:55 +0530

    Fix : Fix style issue in call to action

--- patterns/call-to-action.php
+++ patterns/call-to-action.php
@@ -7,22 +7,26 @@
  * Keywords: call to action
  */
 ?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"bottom":"var:preset|spacing|70"}},"border":{"radius":"20px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-primary-background-color has-background" style="border-radius:20px;margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-    <!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}},"textColor":"secondary","fontSize":"xxxlarge"} -->
-    <h2 class="wp-block-heading has-text-align-center has-secondary-color has-text-color has-link-color has-xxxlarge-font-size" style="padding-bottom:var(--wp--preset--spacing--30)"><?php esc_html_e('Focus on your business growth with Round', 'upshift'); ?></h2>
-    <!-- /wp:heading -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"backgroundColor":"bg-color","textColor":"secondary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-secondary-color has-bg-color-background-color has-text-color has-background has-link-color" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}},"border":{"radius":"20px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group alignwide has-primary-background-color has-background" style="border-radius:20px;padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+        <!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}},"textColor":"secondary","fontSize":"xxxlarge"} -->
+        <h2 class="wp-block-heading has-text-align-center has-secondary-color has-text-color has-link-color has-xxxlarge-font-size" style="padding-bottom:var(--wp--preset--spacing--30)"><?php esc_html_e('Focus on your business growth with Round', 'upshift'); ?></h2>
+        <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"textColor":"secondary","fontSize":"xx-small"} -->
-    <p class="has-text-align-center has-secondary-color has-text-color has-link-color has-xx-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)"><?php esc_html_e('Create, build and ship your products real faster. With your own customized portal. showcase your Business like never before', 'upshift'); ?></p>
-    <!-- /wp:paragraph -->
+        <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"textColor":"secondary","fontSize":"xx-small"} -->
+        <p class="has-text-align-center has-secondary-color has-text-color has-link-color has-xx-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)"><?php esc_html_e('Create, build and ship your products real faster. With your own customized portal. showcase your Business like never before', 'upshift'); ?></p>
+        <!-- /wp:paragraph -->
 
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-    <div class="wp-block-buttons">
-        <!-- wp:button -->
-        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Start a Demo', 'upshift'); ?></a></div>
-        <!-- /wp:button -->
+        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons">
+            <!-- wp:button -->
+            <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Start a Demo', 'upshift'); ?></a></div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
     </div>
-    <!-- /wp:buttons -->
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
