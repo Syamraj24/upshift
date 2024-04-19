938edb16013c9a171ad007fdd0c5f569388dbd70
syam raj <syamraj@awsm.in>
2024-04-19 12:49:55 +0530

    Fix: content pattern style

--- patterns/content.php
+++ patterns/content.php
@@ -26,10 +26,7 @@
 
         <!-- wp:column -->
         <div class="wp-block-column"><!-- wp:group {"style":{"border":{"radius":"20px"},"spacing":{"blockGap":"26px","padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"typography":{"letterSpacing":"-0.01em","lineHeight":1.4,"fontStyle":"normal","fontWeight":"700"}},"backgroundColor":"primary","layout":{"type":"constrained","wideSize":"380px"},"fontSize":"large"} -->
-            <div class="wp-block-group has-primary-background-color has-background has-large-font-size" style="border-radius:20px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);font-style:normal;font-weight:700;letter-spacing:-0.01em;line-height:1.4"><!-- wp:spacer {"height":"11px"} -->
-                <div style="height:11px" aria-hidden="true" class="wp-block-spacer"></div>
-                <!-- /wp:spacer -->
-
+            <div class="wp-block-group has-primary-background-color has-background has-large-font-size" style="border-radius:20px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);font-style:normal;font-weight:700;letter-spacing:-0.01em;line-height:1.4">
                 <!-- wp:heading {"level":4} -->
                 <h4 class="wp-block-heading"><?php esc_html_e('3.2 million', 'upshift'); ?><br><?php esc_html_e('happy customers', 'upshift'); ?></h4>
                 <!-- /wp:heading -->
@@ -49,9 +46,6 @@
                 <!-- wp:heading {"level":4} -->
                 <h4 class="wp-block-heading"><?php esc_html_e('$10 million gross', 'upshift'); ?><br><?php esc_html_e('merchandise volume', 'upshift'); ?></h4>
                 <!-- /wp:heading -->
-                <!-- wp:spacer {"height":"11px"} -->
-                <div style="height:11px" aria-hidden="true" class="wp-block-spacer"></div>
-                <!-- /wp:spacer -->
             </div>
             <!-- /wp:group -->
         </div>
