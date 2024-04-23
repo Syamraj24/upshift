c66e1d184e208f7c041ad801b7869d759da1e66d
Aparna Adhun <aparnajl437@gmail.com>
2024-04-23 15:12:15 +0530

    Add : Pot file regenerated. Add : Added block styles

--- functions.php
+++ functions.php
@@ -20,3 +20,59 @@ endif;
 add_action('wp_enqueue_scripts', 'upshift_styles');
 
 require get_template_directory() . '/includes/setup.php';
+
+
+/**
+ * Register block styles.
+ */
+
+if ( ! function_exists( 'upshift_register_block_styles' ) ) :
+	/**
+	 * Register custom block styles
+	 *
+	 * @since Upshift 1.0
+	 * @return void
+	 */
+	function upshift_register_block_styles() {
+		if ( function_exists( 'register_block_style' ) ) :
+			register_block_style(
+				'core/list',
+				array(
+					'name'            => 'checkmark-list',
+					'label'           => __( 'Checkmark', 'upshift' ),
+					/*
+					* Styles for the custom checkmark list block style
+					*/
+					   'inline_style' => '
+				ul.is-style-checkmark-list {
+					list-style-type: "\2714";
+				}
+
+				ul.is-style-checkmark-list li {
+					padding-inline-start: 1ch;
+				}',
+				)
+			);
+			register_block_style(
+				'core/navigation-link',
+				array(
+					'name'            => 'arrow-link',
+					'label'           => __( 'With arrow', 'upshift' ),
+					/*
+					* Styles for the custom arrow nav link block style
+					*/
+					   'inline_style' => '
+				.is-style-arrow-link .wp-block-navigation-item__label:after {
+					content: "\2197";
+					padding-inline-start: 0.25rem;
+					vertical-align: middle;
+					text-decoration: none;
+					display: inline-block;
+				}',
+				)
+			);
+		endif;
+	}
+endif;
+
+add_action( 'init', 'upshift_register_block_styles' );
